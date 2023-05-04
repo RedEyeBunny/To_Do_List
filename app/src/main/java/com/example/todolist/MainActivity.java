17f306e9fec7ba1dc00a10e9f7c400bf22c61b16
RedEyeBunny <hsreblw3@gmail.com>
2023-05-04 06:00:24 +0530

    New XML file is added.. some files are updated.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private FloatingActionButton mFab;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recyclerview);
+        mFab = findViewById(R.id.floatingActionButton);
     }
 }
--- app/src/main/java/com/example/todolist/SplashActivity.java
+++ app/src/main/java/com/example/todolist/SplashActivity.java
@@ -25,6 +25,5 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         },3000);
-
     }
 }
